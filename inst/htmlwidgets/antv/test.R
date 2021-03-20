22b8a43ac5f3e231e817aa14701fa72ee73fc1b0
GOUYONGCHAO <gouyongchao@gmail.com>
2021-03-21 00:33:12 +0800

    hi

--- inst/htmlwidgets/antv/test.R
+++ inst/htmlwidgets/antv/test.R
@@ -2,3 +2,10 @@ library('jsonlite')
 data<-fromJSON('https://gw.alipayobjects.com/os/antfincdn/aao6XnO5pW/IMDB.json')
 write.csv(data,'~/g2plot/data/IMDB.csv')
 g(data) %>% aes(x = "Revenue (Millions)",y = "Rating",color='Genre',size = '4') %>% scatter()
+g(data,plot_type = 'scatter') %>% aes(x = "Revenue (Millions)",y = "Rating",color='Genre',size = '4')
+
+#line
+data<-fromJSON('https://gw.alipayobjects.com/os/bmw-prod/55424a73-7cb8-4f79-b60d-3ab627ac5698.json')
+
+g(data) %>% aes(x = "year",y = "value",group='category')%>% line()
+
